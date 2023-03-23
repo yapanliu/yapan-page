2a1a9ad51941265e0ca427fbfcaaa39b66385605
yapan_su_linux <yapanliu@yahoo.com>
2023-03-22 23:54:29 -0400

    added one post for ob database media appearance

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,6 +1,17 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Research Highlighted by Syracuse CoE',
+    subtitle: 'ASHRAE Global Occupant Behavior Database',
+    publish: true,
+    paperLink: 'https://doi.org/10.1038/s41597-022-01475-3',
+    image: '/images/projects/ashrae-ob-database-global-contribution.webp',
+    link: 'https://www.linkedin.com/posts/syracuse-university-college-of-engineering-and-computer-science_syracuse-researchers-collaborate-to-create-activity-7044381415873871872-m-6W?utm_source=share&utm_medium=member_desktop',
+    date: '2023-03-22',
+    desc:
+      'Syracuse Center of Excellence Associate Director and Mechanical and Aerospace Engineering Professor Bing Dong and several of his students have compiled research on how building occupants behave – more specifically, how they interact with building systems like windows, doors, light switches, thermostats and fans.',
+  },
+  {
     title: '2022 BAE Best Paper Award',
     subtitle: 'Visualization of Human Mobility Tracking',
     publish: true,
